b83dfc331c8cd98317c9ceb919f09ebd0c8853eb
sherin-nishara <sherinnishara@outlook.com>
2026-04-27 14:50:31 +0530

    Update WebConfig.java

--- app/src/main/java/web/rumers/app/config/WebConfig.java
+++ app/src/main/java/web/rumers/app/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:8080",
-                        "http://localhost:8081"
+                        "https://rumers.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
